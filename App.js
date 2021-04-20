b559f6fbde3cc4572db29769d6325d9ec326b068
Ha young, Kim <jeremiah91@naver.com>
2021-04-20 14:50:49 +0900

    how to request permission for users

--- App.js
+++ App.js
@@ -2,6 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Loading from "./Loading";
 
-export default function App() {
-  return <Loading />;
+import * as Location from 'expo-location';
+import { Alert } from 'react-native';
+
+export default class extends React.Component {
+  state={
+    isLoading: true,
+  }
+  getLocation = async () => {
+    try{
+      await Location.requestForegroundPermissionsAsync();
+      const { coords: { latitude, longitude} } = await Location.getCurrentPositionAsync();
+      this.setState({isLoading:false});
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
