9951df68208f3ebbb1ca401195fe6a6db6b1afbc
Ha young, Kim <jeremiah91@naver.com>
2021-04-21 14:00:54 +0900

    adding icons

--- Weather.js
+++ Weather.js
@@ -1,24 +1,45 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Weather({temp}){
-    return (
-        <View style={styles.container}>
-            <Text>{temp}</Text>
-        </View>
-    )
+export default function Weather({ temp }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
+        <Text style={styles.temp}>{temp}℃</Text>
+      </View>
+      <View style={styles.halfContainer}></View>
+    </View>
+  );
 }
 
-Weather.propTypes={
-    temp:PropTypes.number.isRequired,
-    condition:PropTypes.oneOf(["Thunderstorm", "Drizzle", "Rain", "Snow", "Atmosphere", "Clear", "Clouds"]).isRequired
-}
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+  ]).isRequired,
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 32,
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
